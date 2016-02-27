a7c24b123956ebb714cb23e801a309e45776839b
Snow <daniel@xeno.solutions>
2016-02-27 20:56:13 +0000

    Created the BTPServerEmployeeClient class

---
+++ BankTransmissionProtocol/src/BTP/BTPServerEmployeeClient.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BTP;
+
+import java.net.Socket;
+
+/**
+ *
+ * @author Daniel
+ */
+public class BTPServerEmployeeClient extends BTPServerClient {
+
+    private BTPEmployee employee;
+    public BTPServerEmployeeClient(BTPServer server, Socket client) {
+        super(server, client);
+        this.employee = null;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+    
+    @Override
+    protected void handleSocketInput() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public BTPEmployee getEmployee() {
+        return this.employee;
+    }
+}
