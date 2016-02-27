108bf0268417b1427702aebae6f7f618b59f4fd9
Snow <daniel@xeno.solutions>
2016-02-27 20:59:01 +0000

    Created the BTPServerAdministratorClient class

---
+++ BankTransmissionProtocol/src/BTP/BTPServerAdministratorClient.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BTP;
+
+import java.net.Socket;
+
+/**
+ *
+ * @author Daniel
+ */
+public class BTPServerAdministratorClient extends BTPServerEmployeeClient {
+
+    public BTPServerAdministratorClient(BTPServer server, Socket client) {
+        super(server, client);
+    }
+    
+    @Override
+    public void run() {
+        super.run();
+    }
+    
+    @Override
+    protected void handleSocketInput() {
+         throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
