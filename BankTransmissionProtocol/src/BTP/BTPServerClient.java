d36b3a6b7722a452aa5f77aad8746913369a7b9c
Snow <daniel@xeno.solutions>
2016-02-27 20:46:22 +0000

    Created the BTPServerClient class.

---
+++ BankTransmissionProtocol/src/BTP/BTPServerClient.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BTP;
+
+import java.net.Socket;
+
+/**
+ *
+ * @author Daniel
+ */
+public abstract class BTPServerClient extends BTPClient implements Runnable {
+    private final BTPServer server;
+    
+    public BTPServerClient(BTPServer server, Socket client) {
+        super(client);
+        this.server = server;
+    }
+    
+   
+    @Override
+    public void run() {
+        this.handleSocketInput();
+    }
+    
+    protected abstract void handleSocketInput();
+    
+    public BTPServer getServer() {
+        return this.server;
+    }
+}
