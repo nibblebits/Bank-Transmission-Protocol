10238332883229835639ba3f383ee153a7ebd176
Snow <daniel@xeno.solutions>
2016-02-28 20:57:57 +0000

    Added extra code and created a shutdown method

--- BankTransmissionProtocol/src/BTP/BTPServer.java
+++ BankTransmissionProtocol/src/BTP/BTPServer.java
@@ -5,7 +5,9 @@
  */
 package BTP;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,6 +17,34 @@ public class BTPServer {
     private int port;
     private BTPServerEventHandler eventHandler;
     private ServerSocket server_socket;
+    private ArrayList<BTPClient> client;
     
+    public BTPServer(BTPServerEventHandler eventHandler) {
+        this.port = -1;
+        this.server_socket = null;
+        this.client = new ArrayList<BTPClient>();
+        this.eventHandler = eventHandler;
+    }
     
+    public void listen(int port) throws IOException {
+        this.server_socket = new ServerSocket(port);
+        // Create a new thread that will listen for clients
+        new Thread(new BTPServerClientAcceptor(this, this.server_socket)).start();
+    }
+    
+    public BTPServerEventHandler getEventHandler() {
+        return this.eventHandler;
+    }
+    
+    public void addClient(BTPServerClient client) {
+        this.client.add(client);
+    }
+    
+    public ArrayList<BTPClient> getClients() {
+        return this.client;
+    }
+    
+    public void shutdown() {
+        
+    }
 }
