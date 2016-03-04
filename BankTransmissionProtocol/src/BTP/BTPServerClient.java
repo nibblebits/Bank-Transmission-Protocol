dd4eb1802de29e13668ad010364d333be820bd2a
Snow <daniel@xeno.solutions>
2016-03-04 22:45:32 +0000

    Removed a statement that printed to the screen which was not needed and was originally used for debugging I believe.

--- BankTransmissionProtocol/src/BTP/BTPServerClient.java
+++ BankTransmissionProtocol/src/BTP/BTPServerClient.java
@@ -15,39 +15,38 @@ import java.util.logging.Logger;
  * @author Daniel
  */
 public abstract class BTPServerClient extends BTPClient implements Runnable {
-    
+
     private final BTPServer server;
-    
+
     public BTPServerClient(BTPSystem system, BTPServer server, Socket client) throws IOException {
         super(system, client);
         this.server = server;
     }
-    
-   
+
     @Override
-    public void run()  {
+    public void run() {
         try {
             this.authenticate();
         } catch (Exception ex) {
-            // They failed to authenticate so return.
-            System.err.println("Login failed: " + ex.getMessage());
+            // They failed to authenticate so return
             return;
         }
-        
-        while(true) {
+
+        while (true) {
             try {
                 this.handleSocketInput();
             } catch (Exception ex) {
-                // Something went wrong? Could be anything so log the error and return.
+                // Something went wrong? Could be anything so log the error and then return
                 Logger.getLogger(BTPServerClient.class.getName()).log(Level.SEVERE, null, ex);
                 return;
             }
         }
     }
-    
+
     protected abstract void authenticate() throws Exception;
+
     protected abstract void handleSocketInput() throws Exception;
-    
+
     public synchronized BTPServer getServer() {
         return this.server;
     }
