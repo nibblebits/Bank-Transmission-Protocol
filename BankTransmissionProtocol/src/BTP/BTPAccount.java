0ddf7d723d57538b70b57e035c930499013e7bc5
Snow <daniel@xeno.solutions>
2016-03-07 00:10:13 +0000

    Made the BTPAccount constructor create a new instance of BTPKeyContainer if the value passed is null. This is to help avoid null pointer exceptions

--- BankTransmissionProtocol/src/BTP/BTPAccount.java
+++ BankTransmissionProtocol/src/BTP/BTPAccount.java
@@ -10,30 +10,35 @@ package BTP;
  * @author Daniel
  */
 public class BTPAccount {
+
     private final int customer_id;
     private final int account_no;
     private final String sort_code;
     private final BTPKeyContainer extra;
-    
+
     public BTPAccount(int customer_id, int account_no, String sort_code, BTPKeyContainer extra) {
         this.customer_id = customer_id;
         this.account_no = account_no;
         this.sort_code = sort_code;
-        this.extra = extra;
+        if (extra != null) {
+            this.extra = extra;
+        } else {
+            this.extra = new BTPKeyContainer();
+        }
     }
-    
+
     public int getCustomerId() {
         return this.customer_id;
     }
-    
+
     public int getAccountNumber() {
         return this.account_no;
     }
-    
+
     public String getSortCode() {
         return this.sort_code;
     }
-    
+
     public BTPKeyContainer getExtraDetail() {
         return this.extra;
     }
