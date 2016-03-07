d6a8d189623ea0ac64381d9f9c45a87fa636c71a
Snow <daniel@xeno.solutions>
2016-03-07 00:15:16 +0000

    Made the interface throw a few more exceptions by default. Still not completed

--- BankTransmissionProtocol/src/BTP/BTPServerEventHandler.java
+++ BankTransmissionProtocol/src/BTP/BTPServerEventHandler.java
@@ -11,11 +11,11 @@ package BTP;
  */
 public interface BTPServerEventHandler {
     public void customerLogin(CustomerLoginEvent event) throws BTP.exceptions.BTPPermissionDeniedException, BTP.exceptions.BTPDataException, BTP.exceptions.BTPAccountNotFoundException, Exception;
-    public void createBankAccount(CreateNewBankAccountEvent event);
-    public void createCustomer(CreateCustomerEvent event); 
-    public void setDailyOverdrawnCharge(SetDailyOverdrawnChargeEvent event);
-    public BTPAccountType[] getBankAccountTypes(GetBankAccountTypesEvent event);
-    public BTPAccount[] getBankAccountsOfCustomer(GetBankAccountsOfCustomerEvent event);
+    public void createBankAccount(CreateNewBankAccountEvent event) throws BTP.exceptions.BTPPermissionDeniedException, BTP.exceptions.BTPDataException, BTP.exceptions.BTPAccountNotFoundException, BTP.exceptions.BTPInvalidAccountTypeException, Exception;
+    public void createCustomer(CreateCustomerEvent event) throws BTP.exceptions.BTPPermissionDeniedException, BTP.exceptions.BTPDataException, Exception;
+    public void setDailyOverdrawnCharge(SetDailyOverdrawnChargeEvent event) throws BTP.exceptions.BTPPermissionDeniedException, BTP.exceptions.BTPBankNotFoundException, BTP.exceptions.BTPDataException, BTP.exceptions.BTPInvalidAccountTypeException, Exception;
+    public BTPAccountType[] getBankAccountTypes(GetBankAccountTypesEvent event) throws BTP.exceptions.BTPPermissionDeniedException, BTP.exceptions.BTPDataException, Exception;
+    public BTPAccount[] getBankAccountsOfCustomer(GetBankAccountsOfCustomerEvent event) throws BTP.exceptions.BTPPermissionDeniedException, BTP.exceptions.BTPDataException, Exception;
     public void setBankAccountsOverdraftLimit(SetBankAccountOverdraftLimitEvent event);
     public void transfer(RemoteTransferEvent event) throws BTP.exceptions.BTPAccountNotFoundException, BTP.exceptions.BTPBankNotFoundException, BTP.exceptions.BTPDataException, BTP.exceptions.BTPInvalidAccountTypeException, BTP.exceptions.BTPPermissionDeniedException;
     public void transfer(LocalTransferEvent event) throws BTP.exceptions.BTPAccountNotFoundException, BTP.exceptions.BTPBankNotFoundException, BTP.exceptions.BTPDataException, BTP.exceptions.BTPInvalidAccountTypeException, BTP.exceptions.BTPPermissionDeniedException;
