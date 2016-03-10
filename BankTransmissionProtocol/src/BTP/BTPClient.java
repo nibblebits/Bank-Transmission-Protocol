5e1a05b8956375a8fce1b0d9c575f0794b6cd9b6
Snow <daniel@xeno.solutions>
2016-03-10 03:37:38 +0000

    Added readAccountFromSocket and writeAccountToSocket methods. These methods have been created as sending and reading bank accounts over the socket will be common so it makes sense to put these methods in the BTPClient class so all its children can use these methods.

--- BankTransmissionProtocol/src/BTP/BTPClient.java
+++ BankTransmissionProtocol/src/BTP/BTPClient.java
@@ -15,18 +15,19 @@ import java.net.Socket;
  *
  * @author Daniel
  */
-
 public abstract class BTPClient {
+
     public static int Customer = 0;
     public static int Employee = 1;
     public static int Transfer = 2;
     public static int Administrator = 3;
-    
+
     private Socket socket;
     private BTPSystem system;
     private boolean authenticated;
     private PrintStream output;
     private BufferedReader input;
+
     public BTPClient(BTPSystem system, Socket socket) throws IOException {
         this.system = system;
         this.socket = socket;
@@ -34,34 +35,65 @@ public abstract class BTPClient {
         this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.authenticated = false;
     }
-    
+
     protected synchronized PrintStream getPrintStream() {
         return this.output;
     }
-    
+
     protected synchronized BufferedReader getBufferedReader() {
         return this.input;
     }
-    
+
     public synchronized boolean isAuthenticated() {
         return this.authenticated;
     }
-    
+
     protected synchronized void setAuthenticated(boolean authenticated) {
         this.authenticated = authenticated;
     }
-    
+
     protected synchronized Socket getSocket() {
         return this.socket;
     }
-    
+
     public BTPSystem getSystem() {
         return this.system;
     }
-    
+
     public void shutdown() throws IOException {
-        if (this.socket != null)  {
+        if (this.socket != null) {
             this.socket.close();
         }
+    }
+
+    protected BTPAccount readAccountFromSocket() throws IOException {
+        int customer_id = Integer.parseInt(this.getBufferedReader().readLine());
+        int account_no = Integer.parseInt(this.getBufferedReader().readLine());
+        String sort_code = this.getBufferedReader().readLine();
+        int total_options = this.getBufferedReader().read();
+        BTPKeyContainer extra = new BTPKeyContainer();
+        for (int b = 0; b < total_options; b++) {
+            BTPKey key = new BTPKey(
+                    this.getBufferedReader().readLine(),
+                    this.getBufferedReader().readLine()
+            );
+            extra.addKey(key);
+        }
+
+        return new BTPAccount(customer_id, account_no, sort_code, extra);
+    }
+
+    protected void writeAccountToSocket(BTPAccount account) {
+        this.getPrintStream().println(account.getCustomerId());
+        this.getPrintStream().println(account.getAccountNumber());
+        this.getPrintStream().println(account.getSortCode());
+        this.getPrintStream().write(account.getExtraDetail().getTotalKeys());
+        for (int i = 0; i < account.getExtraDetail().getTotalKeys(); i++) {
+            BTPKey key = account.getExtraDetail().getKey(i);
+            this.getPrintStream().println(key.getIndexName());
+            this.getPrintStream().println(key.getValue());
+        }
+        
+        this.getPrintStream().flush();
     }
 }
