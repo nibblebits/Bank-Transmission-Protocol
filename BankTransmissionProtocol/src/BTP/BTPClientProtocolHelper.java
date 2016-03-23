e9d6c315b8b9d5fa782e8a09a245216e219d0d2e
Snow <daniel@xeno.solutions>
2016-03-23 03:18:52 +0000

    Implemented the bodys of the defined methods

--- BankTransmissionProtocol/src/BTP/BTPClientProtocolHelper.java
+++ BankTransmissionProtocol/src/BTP/BTPClientProtocolHelper.java
@@ -5,7 +5,11 @@
  */
 package BTP;
 
+import BTP.exceptions.BTPDataException;
+import BTP.exceptions.BTPPermissionDeniedException;
+import BTP.exceptions.BTPUnknownException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Date;
@@ -16,19 +20,54 @@ import java.util.Date;
  */
 public class BTPClientProtocolHelper extends BTPProtocolHelper {
 
-    public BTPClientProtocolHelper(BTPSystem system, BufferedReader input, PrintStream output, Socket socket) {
-        super(system, input, output, socket);
+    public BTPClientProtocolHelper(BTPSystem system, BufferedReader input, PrintStream output) {
+        super(system, input, output);
     }
-    
-    public void transfer(BTPAccount account_from, BTPAccount account_to, double amount) {
-        
+
+    public void transfer(BTPAccount account_from, BTPAccount account_to, double amount) throws BTPPermissionDeniedException, BTPDataException, Exception {
+        this.getPrintStream().write(BTPOperation.TRANSFER);
+        this.writeAccountToSocket(account_from);
+        this.writeAccountToSocket(account_to);
+        this.getPrintStream().println(Double.toString(amount));
+        int response = this.getBufferedReader().read();
+        if (response != BTPResponseCode.ALL_OK) {
+            String message = this.getBufferedReader().readLine();
+            this.getSystem().throwExceptionById(response, message);
+        }
     }
-    
-    public double getBalance(BTPAccount account) {
+
+    public double getBalance(BTPAccount account) throws BTPPermissionDeniedException, BTPDataException, Exception {
+        this.getPrintStream().write(BTPOperation.GET_BALANCE);
+        this.getPrintStream().println(account.getAccountNumber());
+        int response = this.getBufferedReader().read();
+        if (response == BTPResponseCode.ALL_OK) {
+            return Double.valueOf(this.getBufferedReader().readLine());
+        } else {
+            String message = this.getBufferedReader().readLine();
+            this.getSystem().throwExceptionById(response, message);
+        }
+
         return 0;
     }
-    
-    public BTPTransaction[] getTransactions(BTPAccount account, Date from, Date to) {
-        return null;
+
+    public BTPTransaction[] getTransactions(BTPAccount account, Date from, Date to) throws BTPPermissionDeniedException, BTPDataException, BTPUnknownException, IOException, Exception {
+        BTPTransaction[] transactions = null;
+        this.getPrintStream().write(BTPOperation.GET_TRANSACTIONS);
+        this.getPrintStream().println(account.getAccountNumber());
+        this.getPrintStream().println(from.getTime());
+        this.getPrintStream().println(to.getTime());
+        int response = this.getBufferedReader().read();
+        if (response == BTPResponseCode.ALL_OK) {
+            int total_transactions = this.getBufferedReader().read();
+            transactions = new BTPTransaction[total_transactions];
+
+            for (int i = 0; i < total_transactions; i++) {
+                transactions[i] = this.readTransactionFromSocket();
+            }
+        } else {
+            String message = this.getBufferedReader().readLine();
+            this.getSystem().throwExceptionById(response, message);
+        }
+        return transactions;
     }
 }
