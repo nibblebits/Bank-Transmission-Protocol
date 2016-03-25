8808e3ce36aeb79d5902a4adbe299185ccbe1f93
Snow <daniel@xeno.solutions>
2016-03-25 12:52:32 +0000

    Added ability to make transfers and fixed a bug with the authentication.

--- BankTransmissionProtocol/src/BTP/BTPServerEmployeeClient.java
+++ BankTransmissionProtocol/src/BTP/BTPServerEmployeeClient.java
@@ -5,8 +5,12 @@
  */
 package BTP;
 
+import BTP.exceptions.BTPDataException;
+import BTP.exceptions.BTPPermissionDeniedException;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -33,17 +37,40 @@ public class BTPServerEmployeeClient extends BTPServerClient {
         try {
             this.getServer().getEventHandler().employeeLogin(new EmployeeLoginEvent(this, employee_id, password));
             this.setAuthenticated(true);
+            this.getPrintStream().write(BTPResponseCode.ALL_OK);
         } catch (Exception ex) {
             this.getProtocolHelper().sendExceptionResponseOverSocket(ex);
         }
     }
 
     @Override
-    protected void handleOperation(int opcode) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    protected void handleOperation(int opcode) throws IOException {
+        switch (opcode) {
+            case BTPOperation.TRANSFER: {
+                int account_from_no = Integer.parseInt(this.getBufferedReader().readLine());
+
+                try {
+                    BTPAccount account_from = this.getBankAccount(account_from_no);
+                    BTPAccount account_to = this.getProtocolHelper().readAccountFromSocket(true);
+                    if (account_from == null) {
+                        throw new BTP.exceptions.BTPAccountNotFoundException("The account you are sending from does not exist");
+                    }
+
+                    double amount = Double.parseDouble(this.getBufferedReader().readLine());
+                    this.getProtocolHelper().handleTransferEnquiry(account_from, account_to, amount);
+                } catch (Exception ex) {
+                    this.getProtocolHelper().sendExceptionResponseOverSocket(ex);
+                }
+                break;
+            }
+        }
     }
 
     public BTPEmployee getEmployee() {
         return this.employee;
+    }
+
+    protected BTPAccount getBankAccount(int id) throws BTPPermissionDeniedException, BTPDataException, Exception {
+        return this.getServer().getEventHandler().getBankAccount(new GetBankAccountEvent(this, id));
     }
 }
