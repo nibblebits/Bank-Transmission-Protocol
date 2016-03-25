ef800cb1d0dc50c84c13519953b7978ba5ced79f
Snow <daniel@xeno.solutions>
2016-03-25 07:41:07 +0000

    Added Java doc

--- BankTransmissionProtocol/src/BTP/BTPAccountType.java
+++ BankTransmissionProtocol/src/BTP/BTPAccountType.java
@@ -6,22 +6,35 @@
 package BTP;
 
 /**
+ * <h1>Holds a bank account type</h1>
+ * This class holds a type of bank account, only the id of the type and the name
+ * are stored.
  *
+ * @version 1.0
  * @author Daniel
  */
 public class BTPAccountType {
+
     private final int id;
     private final String name;
-    
+
     public BTPAccountType(int id, String name) {
         this.id = id;
         this.name = name;
     }
-    
+
+    /* 
+     This method returns the id of the bank account type.
+     @return int Returns the id of this BTPAccountType
+     */
     public int getId() {
         return this.id;
     }
-    
+
+    /* 
+     This method returns the name of the bank account type
+     @return String Returns the name of the BTPAccountType
+     */
     public String getName() {
         return this.name;
     }
