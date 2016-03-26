31077a959a5639142e8ffa5ad1df01ad38dd57a6
Snow <daniel@xeno.solutions>
2016-03-26 10:11:52 +0000

    Created the DBEmployee class to hold employee information

---
+++ BankServer/src/bankserver/DBEmployee.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankserver;
+
+import BTP.BTPKeyContainer;
+import BTP.BTPUser;
+
+/**
+ *
+ * @author Daniel
+ */
+public class DBEmployee extends BTPUser{
+
+    private String password;
+    public DBEmployee(int user_id, String title, String firstname, String middlename, String surname, BTPKeyContainer extra, String password) {
+        super(user_id, title, firstname, middlename, surname, extra);
+        this.password = password;
+    }
+   
+    public String getPassword() {
+        return this.password;
+    }
+}
